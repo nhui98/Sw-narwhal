26d9f3476f046dd3f3b3d932e3b2bb723747a74d
Nathan <hui.n1998@gmail.com>
2022-06-18 01:24:09 +0100

    add fill star svg

---
+++ client/src/assets/iconComponents/IconStarFill.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { SVGProps } from "react";
+
+const SvgIconStarFill = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width="1em"
+    height="1em"
+    viewBox="0 0 17 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M11.86 15.154 8 13.125l-3.86 2.03c-.726.386-1.591-.236-1.45-1.055l.737-4.299L.303 6.757a1 1 0 0 1 .555-1.706l4.316-.627L7.104.512c.337-.683 1.458-.683 1.794 0l1.93 3.911 4.317.627a1.001 1.001 0 0 1 .555 1.706l-3.124 3.045.736 4.3c.142.822-.726 1.435-1.451 1.053Z"
+      fill="#F8CE43"
+    />
+  </svg>
+);
+
+export default SvgIconStarFill;
--- client/src/assets/iconComponents/index.ts
+++ client/src/assets/iconComponents/index.ts
@@ -10,5 +10,6 @@ export { default as IconTeams } from "./IconTeams";
 export { default as SwLogoWhite } from "./SwLogoWhite";
 export { default as IconSearch } from "./IconSearch";
 export { default as IconStar } from "./IconStar";
+export { default as IconStarFill } from "./IconStarFill";
 export { default as IconLeadsSmall } from "./IconLeadsSmall";
 export { default as IconConversationsSmall } from "./IconConversationsSmall";
