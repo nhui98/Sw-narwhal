5e1a1dbeb805a9c274e3bb52270ddfcfb87fa0cb
Nathan <hui.n1998@gmail.com>
2022-06-20 00:22:58 +0100

    refactor side bar components and styling

--- client/src/components/Layout/LayoutComponent.tsx
+++ client/src/components/Layout/LayoutComponent.tsx
@@ -1,47 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../Sidebar/Sidebar";
 import Navbar from "../Navbar/Navbar";
-import {
-  IconCampaign,
-  IconTeams,
-  IconLeads,
-  IconReports,
-  IconHelp,
-} from "../../assets/iconComponents";
-
-const icons = [
-  {
-    id: "1",
-    name: "campaign",
-    Icon: IconCampaign,
-  },
-  {
-    id: "2",
-    name: "teams",
-    Icon: IconTeams,
-  },
-  {
-    id: "3",
-    name: "leads",
-    Icon: IconLeads,
-  },
-  {
-    id: "4",
-    name: "reports",
-    Icon: IconReports,
-  },
-  {
-    id: "5",
-    name: "help",
-    Icon: IconHelp,
-  },
-];
+import { sidebarIconsData } from "../../data/sidebarIconsData";
 
 export default function LayoutComponent() {
   return (
     <div className="layout">
-      <div className="sidebar-component">
-        <Sidebar icons={icons} />
+      <div className="sidebar-wrapper">
+        <Sidebar icons={sidebarIconsData} />
       </div>
       <div className="main-container">
         <div className="navbar-component">
---
+++ client/src/data/sidebarIconsData.ts
@@ -0,0 +1,35 @@
+import {
+  IconCampaign,
+  IconTeams,
+  IconLeads,
+  IconReports,
+  IconHelp,
+} from "../assets/iconComponents";
+
+export const sidebarIconsData = [
+  {
+    id: "1",
+    name: "campaign",
+    Icon: IconCampaign,
+  },
+  {
+    id: "2",
+    name: "teams",
+    Icon: IconTeams,
+  },
+  {
+    id: "3",
+    name: "leads",
+    Icon: IconLeads,
+  },
+  {
+    id: "4",
+    name: "reports",
+    Icon: IconReports,
+  },
+  {
+    id: "5",
+    name: "help",
+    Icon: IconHelp,
+  },
+];
