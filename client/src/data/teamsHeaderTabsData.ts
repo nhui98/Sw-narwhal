037d547976b61b7c646bc83cd8fef3401fea3097
Nathan <hui.n1998@gmail.com>
2022-06-20 01:37:26 +0100

    move header tabs data to its own file

---
+++ client/src/data/teamsHeaderTabsData.ts
@@ -0,0 +1,14 @@
+export const tabs = [
+  {
+    title: "All",
+    to: "/teams",
+  },
+  {
+    title: "Favorites",
+    to: "/teams/favourites",
+  },
+  {
+    title: "Archived",
+    to: "/teams/archived",
+  },
+];
